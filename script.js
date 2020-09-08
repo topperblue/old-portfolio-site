80da1a0a5a5593c19ec3fb7a2c7c9712f8f6b483
topperblue <67169800+topperblue@users.noreply.github.com>
2020-09-08 18:04:23 -0400

    Version 1.2
    -added Contact page
    -changed font styles
    -added a welcome message
    -added text to About page

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
- $(document).ready(function() {
-            $(".menu-icon").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-            });
-      });
-
-      // Scrolling Effect
-
-      $(window).on("scroll", function() {
-            if($(window).scrollTop()) {
-                  $('nav').addClass('black');
-            }
-
-            else {
-                  $('nav').removeClass('black');
-            }
-      })
+ $(document).ready(function () {
+ 	$(".menu-icon").on("click", function () {
+ 		$("nav ul").toggleClass("showing");
+ 	});
+ });
